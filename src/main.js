6566c253e672c9e7fea79ba899a2f43b5881c78a
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-07-29 14:34:49 +0300

    Added vuex and vue-router dependencies.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store.js";
+import router from "./router.js";
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: h => h(App),
+  router: router,
+  store
+});
